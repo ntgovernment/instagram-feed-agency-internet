eaafe1961d00e587d6871683822c948272a3da2a
Roy Galet <roy.galet@nt.gov.au>
2026-01-30 21:51:21 +0930

    Implement Bootstrap 4 modular CSS implementation

--- src/index.js
+++ src/index.js
@@ -343,9 +343,9 @@ class InstagramFeed {
               </div>
             </div>
             <div class="instagram-modal__footer">
-              <a href="#" target="_blank" rel="noopener noreferrer" class="instagram-modal__link" id="modal-instagram-link">
+              <a href="#" target="_blank" rel="noopener noreferrer" class="btn btn-primary" id="modal-instagram-link">
                 View on Instagram
-                <svg class="instagram-modal__link-icon" width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                   <polyline points="15 3 21 3 21 9" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                   <line x1="10" y1="14" x2="21" y2="3" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
